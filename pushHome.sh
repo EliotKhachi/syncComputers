59debb5a51b583bfc6d46b3e22e31f516395b265
eliot <eliotkh12@gmail.com>
2024-03-08 21:39:07 -0800

    Pushed backup from konoha to server on Fri Mar  8 09:39:07 PM PST 2024

--- pushHome.sh
+++ pushHome.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
 
 ## Update snap.txt and apt.txt
+echo "Updating snap.txt..."  
 snap list | awk 'NR>1 {print $1}' > snap.txt
+sleep 1 
+echo "Updating apt.txt..."  
 aptitude search '~i!~M' -F '%p' > apt.txt
+sleep 1
 
 DISK_MOUNT_PATH="/mnt/backup"
 DISK_DEVICE="/dev/sda"
 repo="/home/$USER/Public/repos/syncComputers/"
 
-## Sync directories to backup server. Exclude directories that are not needed.
-sudo rsync -avxz -e "ssh -i /home/$USER/.ssh/rpi" --log-file=$repo/push.log --partial-dir=partial \
+## Push home directories to backup server. Exclude directories that are not needed.
+echo "Pushing home directory..."
+sudo rsync -avxzq -e "ssh -i /home/$USER/.ssh/rpi" --log-file=$repo/push.log --partial-dir=partial \
     --exclude-from=exclude.txt \
     /home/$USER/ $PI_USERNAME@$PI_ADDRESS:$DISK_MOUNT_PATH/home/$USER/
 
 ## Update git repository
-git -C $repo/syncComputers add .
-git -C $repo commit -m "Computers synced on $(date)"
-git -C $repo push
+git -C $repo add .
+git -C $repo commit -m "Pushed backup from $HOSTNAME to server on $(date)"
